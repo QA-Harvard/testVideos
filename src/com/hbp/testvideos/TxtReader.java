158b31364a81c7bde52b58f65d24baf655fa6cd4
Jun.Chen <jun.chen@harvardbusiness.org>
2016-03-30 19:53:16 -0400

    Added paragraph detection code for txt files.  Added p tag for text paragraph format.

--- src/com/hbp/testvideos/TxtReader.java
+++ src/com/hbp/testvideos/TxtReader.java
@@ -19,15 +19,41 @@ public class TxtReader {
         FileInputStream fis = new FileInputStream(fullFilePath);
         reader = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
         String line;
-
+        int lineCount =1;
         while ((line = reader.readLine()) != null) {
-            // process the line- just appending to the buffer for now.
 
-            //removing bomb:
+            // process the line- just appending to the buffer for now.
+            if(lineCount == 1 ){
+                txtContentBuffer =  startParagraph(txtContentBuffer, true);
+            }else if(line.startsWith("\t") && lineCount > 1){
+                txtContentBuffer =  startParagraph(txtContentBuffer, false);
+            }
+
+            //Might need to removing bomb in the future: but ok for now.
             txtContentBuffer.append(line);
 
+            lineCount++;
         }
 
+        //Add last paragraph tag before returning.
+        txtContentBuffer = endParagraph(txtContentBuffer);
+
+        return txtContentBuffer;
+    }
+
+    public StringBuffer startParagraph(StringBuffer txtContentBuffer, Boolean firstParagraph){
+
+        if(firstParagraph){
+            txtContentBuffer.append("<P>");
+        }else{
+            txtContentBuffer.append("</P><P>");
+        }
+        return txtContentBuffer;
+
+    }
+
+    public StringBuffer endParagraph(StringBuffer txtContentBuffer){
+        txtContentBuffer.append("</P>");
         return txtContentBuffer;
     }
 }
